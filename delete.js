ab01883063f086a79a172adb69685f611f786d33
Colleen <colleenhiggins87@gmail.com>
2019-12-01 20:33:43 -0800

    delete endpoint added

---
+++ delete.js
@@ -0,0 +1,18 @@
+import * as dyanmoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context) {
+  const params = {
+    TableName: process.env.tableName,
+    Key: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      noteId: event.pathParameters.id
+    }
+  }
+  try {
+    await dyanmoDbLib.call("delete", params)
+    return success({status: true})
+  } catch(e){
+    return failure({status:false})
+  }
+}
