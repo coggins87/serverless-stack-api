acbd6d018761fa6aaa7775c14fc29ecc9db39cf8
Colleen <colleenhiggins87@gmail.com>
2019-12-04 16:08:12 -0800

    adding updated date to db

--- update.js
+++ update.js
@@ -11,10 +11,11 @@ export async function main(event, context) {
     },
     // 'UpdateExpression' defines the attributes to be updated
     // 'ExpressionAttributeValues' defines the value in the update expression
-    UpdateExpression: "SET content = :content, attachment = :attachment",
+    UpdateExpression: "SET content = :content, attachment = :attachment, updatedOn = :updatedOn",
     ExpressionAttributeValues: {
       ":attachment": data.attachment || null,
-      ":content": data.content || null
+      ":content": data.content || null,
+      ":updatedOn": Date.now()
     },
     // 'ReturnValues' specifies if and how to return the item's attributes,
     // where ALL_NEW returns all attributes of the item after the update; you
